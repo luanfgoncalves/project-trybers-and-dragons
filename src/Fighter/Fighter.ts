cad4ae681cc52fb69d76dc01963f5046f4b991d5
luanfgoncalves <luanfgoncalves@outlook.com>
2023-01-20 17:41:14 -0300

    FEAT: Req 6 completo; FIX: remove os espaços antes da tipagem em Fighter.ts

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -5,14 +5,14 @@ import Energy from '../Energy';
 // type Fighter = 'mana' | 'stamina';
 
 export default interface Fighter {
-  lifePoints: number;
-  strength: number;
-  defense: number; 
-  energy?: Energy; // stamina ou mana
+  lifePoints:number;
+  strength:number;
+  defense:number; 
+  energy?:Energy; // stamina ou mana
   attack(enemy:Fighter):void;
   special(enemy?:Fighter):void;
   levelUp():void;
-  recieveDamage(attackPoints:number):number;
+  receiveDamage(attackPoints:number):number;
 }
 
 // export default Fighter;
